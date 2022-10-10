13eaf45a1ce16f737087b30d92d4d22e960e51ec
vlad <khalvlalove@gmail.com>
2022-10-10 18:03:58 +0400

    refactor session name

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -2,19 +2,19 @@ import os
 
 from pytest_voluptuous import S
 from schemas.users import user, users, create_user
-from utils.sessions import session
+from utils.sessions import reqres
 
 
 def test_list_users_validation():
     page = 2
-    response = session().get("/users", params={"page": page})
+    response = reqres().get("/users", params={"page": page})
 
     assert response.status_code == 200
     assert S(users) == response.json()
 
 
 def test_single_user_validation():
-    response = session().get("/users/2")
+    response = reqres().get("/users/2")
 
     assert S(user) == response.json()
 
@@ -23,14 +23,14 @@ def test_create_user_validation():
     name = "morph"
     job = "qa"
     user_data = {"name": name, "job": job}
-    response = session().post("/users", json=user_data)
+    response = reqres().post("/users", json=user_data)
 
     assert response.status_code == 201
     assert S(create_user) == response.json()
 
 
 def test_delete_user():
-    response = session().delete("/users/2")
+    response = reqres().delete("/users/2")
 
     assert response.status_code == 204
 
@@ -39,7 +39,7 @@ def test_register_user():
     EMAIL = os.getenv('email')
     PASSWORD = os.getenv('password')
     user_credentials = {"email": EMAIL, "password": PASSWORD}
-    response = session().post("/register", json=user_credentials)
+    response = reqres().post("/register", json=user_credentials)
 
     assert response.status_code == 200
     assert len(response.json()["token"]) == 17
--- utils/sessions.py
+++ utils/sessions.py
@@ -2,6 +2,6 @@ import os
 from utils.requests_helpers import BaseSession
 
 
-def session() -> BaseSession:
+def reqres() -> BaseSession:
     reqres_url = os.getenv('base_url')
     return BaseSession(base_url=reqres_url)
